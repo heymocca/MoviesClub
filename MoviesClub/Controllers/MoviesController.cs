66866be8301d0724368758df241720bbe54ea6c8
mocca <moccababy777@gmail.com>
2018-04-03 23:20:36 +1200

    MoviesClub first commit

---
+++ MoviesClub/Controllers/MoviesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MoviesClub.Models;
+
+namespace MoviesClub.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+            return View(movie);
+
+        }
+
+        public ActionResult Edit(int movieId)
+        {
+            return Content("movieId=" + movieId);
+        }
+    }
+}
---
+++ MoviesClub/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MoviesClub.Startup))]
+namespace MoviesClub
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
