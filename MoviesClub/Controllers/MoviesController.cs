dddd568381d2c04a2766970ab222341dec691e19
mocca <moccababy777@gmail.com>
2018-04-04 23:06:09 +1200

    Added ActionResult

--- MoviesClub/Controllers/MoviesController.cs
+++ MoviesClub/Controllers/MoviesController.cs
@@ -17,9 +17,21 @@ namespace MoviesClub.Controllers
 
         }
 
-        public ActionResult Edit(int movieId)
+        public ActionResult Edit(int id)
         {
-            return Content("movieId=" + movieId);
+            return Content("moveId=" + id);
         }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+
     }
 }
